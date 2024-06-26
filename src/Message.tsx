a6c4d9a1978af0dc2b84704f5e865e7638f67780
gomdopi <io.gomdopi@gmail.com>
2024-06-25 21:02:22 -0700

    ListGroup component with state

---
+++ src/Message.tsx
@@ -0,0 +1,6 @@
+function Message() {
+  const name = 'Daniel'
+  return <h1>Hello {name}</h1>
+}
+
+export default Message
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function ListGroup() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const [selectedIdx, setSelectedIdx] = useState(-1);
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={selectedIdx === index ? "list-group-item active" : "list-group-item"}
+            key={item}
+            onClick={() => setSelectedIdx(index)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
