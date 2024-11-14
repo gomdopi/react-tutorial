8e7359d6265baccf652a58cb9b431a48201c8dee
gomdopi <io.gomdopi@gmail.com>
2024-11-14 13:59:50 -0800

    finished up to adding order toggle for history

--- src/App.js
+++ src/App.js
@@ -1,25 +1,142 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+function Square({ value, highlight, onSquareClick }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button className={`square ${highlight ? "highlight" : ""}`} onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board({ xIsNext, squares, status, onPlay }) {
+  function handleClick(i) {
+    if (squares[i] || status.winner) return;
+    const nextSquares = squares.slice();
+    nextSquares[i] = xIsNext ? "X" : "O";
+    onPlay(nextSquares);
+  }
+
+  const statusMsg = status.winner
+    ? `Winner: ${status.winner}`
+    : `Next player: ${xIsNext ? "X" : "O"}`;
+
+  function renderSquare(i) {
+    return <Square key={i} value={squares[i]} onSquareClick={() => handleClick(i)} />;
+  }
+
+  function renderRow(n) {
+    const rows = [];
+    for (let i = n; i < n + 3; i++) {
+      rows.push(renderSquare(i));
+    }
+
+    return rows;
+  }
+
+  function renderBoard() {
+    const boardRows = [];
+    for (let i = 0; i < 3; i++) {
+      boardRows.push(
+        <div key={i} className="board-row">
+          {renderRow(i * 3)}
+        </div>
+      );
+    }
+
+    return boardRows;
+  }
+
+  return (
+    <>
+      <div className="status">{statusMsg}</div>
+      {renderBoard()}
+    </>
+  );
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null), move: 0 }]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[history.findIndex(({ _, move }) => move === currentMove)].squares;
+  const [descending, setDescending] = useState(true);
+  const toggleDesc = `Change to ${descending ? "▲▲▲▲▲" : "▼▼▼▼▼"}`;
+  const status = calculateWinner(currentSquares);
+
+  function handlePlay(nextSquares) {
+    const nextMove = currentMove + 1;
+    const nextHistory = descending
+      ? [...history.slice(0, nextMove), { squares: nextSquares, move: nextMove }]
+      : [
+          { squares: nextSquares, move: nextMove },
+          ...history.slice(history.findIndex(({ _, move }) => move === currentMove)),
+        ];
+    setCurrentMove(nextMove);
+    setHistory(nextHistory);
+  }
+
+  function jumpTo(nextMove) {
+    console.log(nextMove);
+    setCurrentMove(nextMove);
+  }
+
+  function handleToggle() {
+    const newDescending = !descending;
+    const newHistory = history.slice().reverse();
+
+    setDescending(newDescending);
+    setHistory(newHistory);
+  }
+
+  const moves = history.map(({ _, move }) => {
+    let description = move > 0 ? `move #${move}` : "game start";
+
+    return (
+      <li key={move}>
+        {move === currentMove ? (
+          `You are at ${description}`
+        ) : (
+          <button onClick={() => jumpTo(move)}>Go to {description}</button>
+        )}
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} status={status} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <button onClick={handleToggle}>{toggleDesc}</button>
+        <ol>{moves}</ol>
+      </div>
     </div>
   );
 }
 
-export default App;
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return {
+        winner: squares[a],
+        winningLine: [a, b, c],
+      };
+    }
+  }
+  return {
+    winner: null,
+    winningLine: null,
+  };
+}
